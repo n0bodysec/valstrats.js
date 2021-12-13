c4ff5ab0eb0bf1e92935917811e55c189979f9b2
Nobody <git@n0bodysec.com>
2021-12-13 05:48:25 -0300

    feat(account): add findSavedStrats

--- src/account/index.js
+++ src/account/index.js
@@ -30,6 +30,23 @@ const account = function account(api)
 		});
 	};
 
+	this.findSavedStrats = async (limit = 6, skip = 0) =>
+	{
+		if (api.getAuthenticated() === false)
+		{
+			throw new Error('you are not authenticated');
+		}
+
+		return new Promise((resolve, reject) =>
+		{
+			api.socket.emit('find', constants.Endpoints.Strats, { $limit: limit, $skip: skip, $sort: { createdAt: -1 }, saved: true }, (err, res) =>
+			{
+				if (err) reject(err);
+				else resolve(res);
+			});
+		});
+	};
+
 	this.saveStrat = async (uuid, save = true) =>
 	{
 		if (api.getAuthenticated() === false)
